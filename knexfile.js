9a0cbc1c76b6aef2cda1816bd7e2ca320b0665b9
gabrueks <bolzi.gabriel@gmail.com>
2025-06-18 16:37:52 -0300

    Update database password in knexfile.js for security compliance

--- knexfile.js
+++ knexfile.js
@@ -7,7 +7,7 @@ const config = {
       host: 'localhost',
       port: 3306,
       user: 'root',
-      password: '76252424',
+      password: 'password',
       database: 'bank_interview'
     },
     migrations: {
